6a268f8be809627486f208e1b61c044dc6359b0d
Adam Richie-Halford <richford@uw.edu>
2016-09-16 19:56:32 -0700

    Add brushing to three.js window

--- client/js/3d-brain.js
+++ client/js/3d-brain.js
@@ -8,7 +8,7 @@ var tracks=["Left Thalamic Radiation","Right Thalamic Radiation","Left Corticosp
 var colors = [0x1F77B4, 0xAEC7E8, 0xFF7F0E, 0xFFBB78, 0x2CA02C, 0x98DF8A, 0xD62728, 0xFF9896, 0x9467BD, 0xC5B0D5, 0x8C564B, 0xC49C94, 0xE377C2, 0xF7B6D2, 0x7F7F7F, 0xC7C7C7, 0xBCBD22, 0xDBDB8D, 0x17BECF, 0x9EDAE5];
 var d3colors = ["#1F77B4", "#AEC7E8", "#FF7F0E", "#FFBB78", "#2CA02C", "#98DF8A", "#D62728", "#FF9896", "#9467BD", "#C5B0D5", "#8C564B", "#C49C94", "#E377C2", "#F7B6D2", "#7F7F7F", "#C7C7C7", "#BCBD22", "#DBDB8D", "#17BECF", "#9EDAE5"];
 // highlightColors[i] = (colors[i] + 10 lightness) converted to RGB hex
-var highlightColors = [0x2991DB, 0xD7E4F4, 0xFF9A42, 0xFFD6AD, 0x37C837, 0xBCEAB3, 0xDF5353, 0xFFC8C7, 0xAC8ACC, 0xDDD0E6, 0xA96C60, 0xD5B9B3, 0xECA2D6, 0xFCE3EE, 0x999, 0xE0E0E0, 0xDCDC38, 0xE8E8B5, 0x30D6E8, 0xC7EAF0];
+var highlightColors = [0x2991DB, 0xD7E4F4, 0xFF9A42, 0xFFD6AD, 0x37C837, 0xBCEAB3, 0xDF5353, 0xFFC8C7, 0xAC8ACC, 0xDDD0E6, 0xA96C60, 0xD5B9B3, 0xECA2D6, 0xFCE3EE, 0x090909, 0xE0E0E0, 0xDCDC38, 0xE8E8B5, 0x30D6E8, 0xC7EAF0];
 
 var m = {top: 20, right: 10, bottom: 10, left: 20},
 w = 400 - m.left - m.right,
@@ -29,7 +29,8 @@ var lh, rh;
 
 var groups = new THREE.Object3D();
 var line_material = new THREE.LineBasicMaterial({
-	linewidth: 1.5
+	opacity: 0.5,
+	linewidth: 1.25
 });
 
 line_material.color.setHex( 0x969696 );
@@ -47,6 +48,10 @@ document.body.onmouseup = function() {
   --mouseDown;
 }
 
+// Hard coded FA plot data length
+// TODO: Read this from an input file so that the user can change fidelity
+var faPlotLength = 100;
+
 var showStats = false;
 var stats;
 
@@ -62,7 +67,7 @@ function init() {
 
     Width = container.clientWidth;
 
-    var brainOpacity = 0.1;
+    var brainOpacity = 0.05;
     var lineInitialOpacity = 0.3;
 
     // camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 1, 2000);
@@ -111,7 +116,6 @@ function init() {
                 child.material.opacity = brainOpacity;
                 child.material.depthWrite = false;
                 child.material.transparent = true;
-
             }
         });
 
@@ -143,37 +147,99 @@ function init() {
         for (var key in json) {
             if (json.hasOwnProperty(key)) {
                 var oneBundle = json[key];
-				var combined = new THREE.Geometry();
+				var nFibers = 0;
 
+				// subkeys correspond to individual fibers in each fiber bundle
+				// They may not be consecutive keys depending on the
+				// downsampling of the input data, hence the need for `nFibers`
+				// and `iFiber`
+				//
+				// First loop simply counts the number of fibers in this bundle
+				// and asserts that each individual fiber has been resampled to
+				// the size of the FA plot data.
                 for (var subkey in oneBundle) {
                     if (oneBundle.hasOwnProperty(subkey)) {
-						var geometry = new THREE.Geometry();
-                        var oneStreamLine = oneBundle[subkey];
+						++nFibers;
+						var oneFiber = oneBundle[subkey];
+						if (oneFiber.length !== faPlotLength) {
+							var errMessage = 'Streamlines have unexpected length';
+							if (typeof Error !== 'undefined') {
+								throw new Error(errMessage);
+							}
+							throw errMessage;
+						}
+					}
+				}
 
-                        // draw this stream line in scene
-                        for (var i = 0; i < oneStreamLine.length - 1; i++) {
-                            geometry.vertices.push(new THREE.Vector3(oneStreamLine[i][0], oneStreamLine[i][1], oneStreamLine[i][2]));
-                            geometry.vertices.push(new THREE.Vector3(oneStreamLine[i+1][0], oneStreamLine[i+1][1], oneStreamLine[i+1][2]));
+				// Positions will hold x,y,z vertices for each fiber
+				var positions = new Float32Array(
+						nFibers * (faPlotLength - 1) * 3 * 2);
+
+				// Outer loop is along the length of each fiber.
+				// Inner loop cycles through each fiber group.
+				// This is counterintuitive but we want spatial locality to
+				// be preserved in index locality. This will make brushing
+				// much easier in the end.
+                for (var i = 0; i < faPlotLength - 1; i++) {
+					var iFiber = 0;
+					for (var subkey in oneBundle) {
+						if (oneBundle.hasOwnProperty(subkey)) {
+							var oneFiber = oneBundle[subkey];
+
+							// Vertices must be added in pairs. Later a
+							// line segment will be drawn in between each pair.
+							// This requires some repeat values to have a
+							// continuous line but will allow us to avoid
+							// having the beginning and end of the fiber
+							// connect.
+							var x1 = oneFiber[i][0],
+								y1 = oneFiber[i][1],
+								z1 = oneFiber[i][2];
+
+							var x2 = oneFiber[i+1][0],
+								y2 = oneFiber[i+1][1],
+								z2 = oneFiber[i+1][2];
+
+							positions[i * nFibers * 6 + iFiber * 6 + 0] = x1;
+							positions[i * nFibers * 6 + iFiber * 6 + 1] = y1;
+							positions[i * nFibers * 6 + iFiber * 6 + 2] = z1;
+
+							positions[i * nFibers * 6 + iFiber * 6 + 3] = x2;
+							positions[i * nFibers * 6 + iFiber * 6 + 4] = y2;
+							positions[i * nFibers * 6 + iFiber * 6 + 5] = z2;
+
+							++iFiber;
                         }
-
-						var line = new THREE.LineSegments(geometry, line_material);
-
-						combined.merge(line.geometry, line.matrix);
                     }
                 }
-				var bundleLine = new THREE.LineSegments(combined, line_material);
+
+				// Create a buffered geometry and line segments from these
+				// positions. Buffered Geometry is slightly more performant
+				// and necessary to interact with d3 brushing later on.
+				var bundleGeometry = new THREE.BufferGeometry();
+				bundleGeometry.addAttribute('position',
+						new THREE.BufferAttribute(positions, 3));
+
+				var bundleLine = new THREE.LineSegments(bundleGeometry,
+						line_material);
+
+				// Set scale to match the brain surface,
+				// (determined by trial and error)
 				bundleLine.scale.set(0.05,0.05,0.05);
 
+				// Record some useful info for later
 				bundleLine.name = tracks[ bundleIdx ];
+				bundleLine.nFibers = nFibers;
 				bundleLine.idx = bundleIdx;
 				++bundleIdx;
 
-				console.log(bundleLine);
-
+				// Add to the group of bundle lines.
                 groups.add(bundleLine);
             }
         }
 
+		// Set material properties for each fiber bundle
+		// And add event listeners for mouseover, etc.
         groups.traverse(function (child) {
 			if (child instanceof THREE.LineSegments) {
                 child.material.opacity = lineInitialOpacity;
@@ -212,6 +278,7 @@ function init() {
             }
         });
 
+		// Finally add fiber bundle group to the scene.
   		scene.add(groups);
     });
 
@@ -225,6 +292,7 @@ function init() {
     controls.addEventListener('change', lightUpdate);
 }
 
+// Resize the three.js window on full window resize.
 function onWindowResize() {
     var Width = container.clientWidth;
 
@@ -240,15 +308,36 @@ function animate() {
     controls.update();
 	if (showStats )
 		stats.update();
+
+	// For each fiber bundle update the length of fiber to be plotted
+	// based on the d3 brushes in the FA plots
+	for (var i = 0; i < groups.children.length; i++) {
+		var track = 'track' + i;
+		var lo = Math.floor(bundleBrush[track].brushExtent[0]);
+		var hi = Math.ceil(bundleBrush[track].brushExtent[1]) - 1;
+
+		// loIdx is the low index and count is the number of indices
+		// This is a little sloppy and sometimes the count will be too high
+		// but the visual offset should be minimal.
+		// TODO: Positions come in pairs, with all vertices except the first
+		// and last being repeated. Take this into account to make loIdx and
+		// count correct (not just good enough).
+		var loIdx = lo * groups.children[i].nFibers * 2;
+		var count = (hi - lo) * groups.children[i].nFibers * 2;
+
+		// Set the drawing range based on the brush extent.
+		groups.children[i].geometry.setDrawRange(loIdx, count);
+	}
 }
 
 function lightUpdate() {
     directionalLight.position.copy(camera.position);
 }
 
-// func to highlight specified bundle based on left panel checkboxes
+// Highlight specified bundle based on left panel checkboxes
 function highlightBundle(state, name) {
 
+	// Temporary line material for highlighted bundles
 	var tem_line_material = new THREE.LineBasicMaterial({
 		opacity: 0.5,
 		linewidth: 2.5,
@@ -270,9 +359,10 @@ function highlightBundle(state, name) {
 	}
 }
 
-// func to highlight specified bundle based on mouseover
+// Highlight specified bundle based on mouseover
 function mouseoverBundle(name) {
 
+	// Temporary line material for moused-over bundles
 	var tem_line_material = new THREE.LineBasicMaterial({
 		opacity: 0.75,
 		linewidth: 2,
@@ -283,6 +373,9 @@ function mouseoverBundle(name) {
 
 	if (bundle !== undefined) {
 		tem_line_material.color.setHex( highlightColors[name] );
+		if (bundleBrush['track' + name].brushOn) {
+			tem_line_material.color.setHex( 0x000000 );
+		}
 		bundle.material = tem_line_material;
 		return renderer.render(scene, camera);
 	}
