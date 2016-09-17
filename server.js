ad209fb4f8d46a0df7fcf00267b0c2bc64a3bc10
arokem <arokem@gmail.com>
2016-09-17 16:49:49 -0700

    Let the environment tell you what port to use (if possible).

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var port = 8080;
+// Default to use 8080, unless environment variable is set:
+if (process.env.PORT == undefined) {
+	var port = 8080
+} else{
+	var port = process.env.PORT;
+}
+
+console.log(port)
 
 app.use(express.static(__dirname + '/client'));
 
