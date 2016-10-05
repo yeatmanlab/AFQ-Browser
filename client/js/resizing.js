172a4b857bdfedd959203075f3af5ecf1c8b1849
Adam Richie-Halford <richford@uw.edu>
2016-10-05 14:32:36 -0700

    Add resizing for both e-w and n-s directions

--- client/js/resizing.js
+++ client/js/resizing.js
@@ -1,9 +1,19 @@
-$('.resizable')
+$('.ew-resize')
 	.resizable({
 		handles: "e",
 		create: function( event, ui ) {
             // Prefers an another cursor with two arrows
 			// Choose between "col-resize" and "ew-resize"
             $(".ui-resizable-e").css("cursor","col-resize");
+        }
+	});
+
+$('.ns-resize')
+	.resizable({
+		handles: "s",
+		create: function( event, ui ) {
+            // Prefers an another cursor with two arrows
+			// Choose between "col-resize" and "ns-resize"
+            $(".ui-resizable-s").css("cursor","row-resize");
         }
 	});
