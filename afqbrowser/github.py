e19cbef5e73ecc0cc5c0259b6f3338eaf4d87dae
arokem <arokem@gmail.com>
2017-02-09 16:59:29 -0800

    This is how the repo is created.

--- afqbrowser/github.py
+++ afqbrowser/github.py
@@ -39,7 +39,7 @@ def upload(target, repo_name, uname=None, upass=None):
     u = g.get_user()
     remote = u.create_repo(repo_name)
     # Create the local repo
-
+    r = git.Repo.init(target)
     # Add all of the files to the repo's gh-pages branch
     r.index.add(file_list)
     r.index.commit("Commit everything")
