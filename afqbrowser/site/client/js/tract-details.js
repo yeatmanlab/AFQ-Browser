be713c86b7e085a88b68a48426d7bcef6fa5e4c3
Josh <jsmith07@uw.edu>
2017-02-15 11:31:56 -0800

    Fix nodeKeys slicing

--- afqbrowser/site/client/js/tract-details.js
+++ afqbrowser/site/client/js/tract-details.js
@@ -147,7 +147,8 @@ function buildPlotGui(error, data) {
 
 	afqb.controls.plotsControlBox = new plotsGuiConfigObj();
 
-    var nodeKeys = Object.keys(data[0]).slice(3, -1);
+    var nodeKeys = Object.keys(data[0]).slice(3);
+		console.log(nodeKeys);
     afqb.controls.plotsControlBox.plotKey = nodeKeys[0];
 
     var keyController = plotsGui
