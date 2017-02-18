4ac7c2d836c46637b1da2c3168340266e6e2b2a7
arokem <arokem@gmail.com>
2017-02-18 12:50:00 -0800

    Update micro-version with requirements file, and install_requires variable.

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ opts = dict(name=NAME,
             packages=PACKAGES,
             package_data=PACKAGE_DATA,
             requires=REQUIRES,
+            install_requires=REQUIRES,
             scripts=SCRIPTS)
 
 
