e26ef4960ab4de6f7d5a3bd07d925a886dfdd8dd
Adam Richie-Halford <richford@uw.edu>
2017-05-02 10:01:33 -0700

    Move tract-colors into afqb.global. Update afqb.js to reflect new object hierarchy. Update 3d-brain.js to use new afqb structure.

--- afqbrowser/site/client/js/afqb.js
+++ afqbrowser/site/client/js/afqb.js
@@ -1,25 +1,20 @@
 // Define global `afqb` and set all other global variables as properties of
 // afqb by direct assignment
 var afqb = {
-	settings: {
-		global: {},
-		table: {},
-		plots: {},
-		three: {}
+	table: {
+		settings: {}
 	},
-	data: {
-		table: {},
-		plots: {},
-		three: {}
+	plots: {
+		settings: {}
 	},
-	methods: {
-		global: {
-			queues: {},
-			mouse: {},
-			controls: {}
-		},
-		table: {},
-		plots: {},
-		three: {}
-	}
+	three: {
+		settings: {}
+	},
+	global: {
+		mouse: {},
+		queues: {},
+		controls: {},
+		settings: {},
+		colors: {}
+	},
 };
