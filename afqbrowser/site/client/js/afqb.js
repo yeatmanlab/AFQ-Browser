2a0e44c5f9199667eb8f5650a80e131ccd217d92
Adam Richie-Halford <richford@uw.edu>
2017-05-02 08:59:37 -0700

    WIP: reorganize global afqb object to allow saving state of browser.

---
+++ afqbrowser/site/client/js/afqb.js
@@ -0,0 +1,25 @@
+// Define global `afqb` and set all other global variables as properties of
+// afqb by direct assignment
+var afqb = {
+	settings: {
+		global: {},
+		table: {},
+		plots: {},
+		three: {}
+	},
+	data: {
+		table: {},
+		plots: {},
+		three: {}
+	},
+	methods: {
+		global: {
+			queues: {},
+			mouse: {},
+			controls: {}
+		},
+		table: {},
+		plots: {},
+		three: {}
+	}
+};
