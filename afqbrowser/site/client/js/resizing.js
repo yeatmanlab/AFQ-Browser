4acd78c4ef75ee39c4c383580cf0b5110af71bc5
Adam Richie-Halford <richford@uw.edu>
2017-06-08 19:50:27 -0700

    Fix jslint/jshint warnings in js/resizing.js

--- afqbrowser/site/client/js/resizing.js
+++ afqbrowser/site/client/js/resizing.js
@@ -1,12 +1,17 @@
+// Tell jslint that certain variables are global
+/*global afqb, $*/
+
 $('.ew-resize')
 	.resizable({
 		handles: "e",
-		create: function( event, ui ) {
+		create: function (event, ui) {
+            "use strict";
             // Prefers an another cursor with two arrows
 			// Choose between "col-resize" and "ew-resize"
-            $(".ui-resizable-e").css("cursor","col-resize");
+            $(".ui-resizable-e").css("cursor", "col-resize");
         },
-		resize: function( event, ui) {
+		resize: function (event, ui) {
+            "use strict";
 			afqb.three.onWindowResize();
 		}
 	});
@@ -14,12 +19,14 @@ $('.ew-resize')
 $('.ns-resize')
 	.resizable({
 		handles: "s",
-		create: function( event, ui ) {
+		create: function (event, ui) {
+            "use strict";
             // Prefers an another cursor with two arrows
 			// Choose between "col-resize" and "ns-resize"
-            $(".ui-resizable-s").css("cursor","row-resize");
+            $(".ui-resizable-s").css("cursor", "row-resize");
         },
-		resize: function( event, ui) {
+		resize: function (event, ui) {
+            "use strict";
 			afqb.three.onWindowResize();
 		}
 	});
