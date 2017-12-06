8ad5fffc761c14fd26508f484b8f971139636ebc
arokem <arokem@gmail.com>
2017-12-06 07:17:42 -0800

    Set user to drop `github.io` part of the hostname.

--- afqbrowser/site/client/js/save-settings.js
+++ afqbrowser/site/client/js/save-settings.js
@@ -173,7 +173,7 @@ $('#launch-binder').click(function (e) {
     e.preventDefault();
     // Parse the URL
     var uri = new URI(location.href);
-    var user = uri.hostname();
+    var user = user.hostname().replace('.github.io/', '');
     var repo = uri.directory();
     var binderUrl = '//mybinder.org/v2/gh/' + user + '/' + repo + '/gh-pages?filepath=index.ipynb';
     console.log(binderUrl);
