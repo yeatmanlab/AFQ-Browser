739a512047ab5b33a36c3edc623392ee05c81610
Ariel Rokem <arokem@gmail.com>
2021-06-11 19:48:40 -0700

    Updates CI config and installation machinery.

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
-import os
-from setuptools import setup, find_packages
-PACKAGES = find_packages()
-
-# Get version and release info, which is all stored in shablona/version.py
-ver_file = os.path.join('afqbrowser', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
+from setuptools import setup
+import string
+import os.path as op
+import glob
+from setuptools_scm import get_version
 
 
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=PACKAGES,
-            package_data=PACKAGE_DATA,
-            requires=REQUIRES,
-            install_requires=REQUIRES,
-            scripts=SCRIPTS)
+def local_version(version):
+    """
+    Patch in a version that can be uploaded to test PyPI
+    """
+    scm_version = get_version()
+    if "dev" in scm_version:
+        gh_in_int = []
+        for char in version.node:
+            if char.isdigit():
+                gh_in_int.append(str(char))
+            else:
+                gh_in_int.append(str(string.ascii_letters.find(char)))
+        return "".join(gh_in_int)
+    else:
+        return ""
+
+
+opts = dict(
+    use_scm_version={"root": ".", "relative_to": __file__,
+                     "write_to": op.join("afqbrowser", "version.py"),
+                     "local_scheme": local_version},
+    scripts=[op.join('bin', op.split(f)[-1]) for f in glob.glob('bin/*')])
 
 
 if __name__ == '__main__':
